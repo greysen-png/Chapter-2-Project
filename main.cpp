9d96c289a411b8af0b409da1ca60f7cb8348dc12
Greysen <gr10689@email.vccs.edu>
2024-09-25 22:44:36 +0000

    Added header comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
-#include <iostream>
-
-int main() { std::cout << "Hello World!\n"; }
+// This is the main branch for the next three mini programs
+// I'll be doing
+// Greysen Roy-Pascoe
+// 09/25/2024
+// Chapter 2 Programming Project
