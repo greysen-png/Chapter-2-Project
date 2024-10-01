575993c468c6c7e0f35e59b22fc3a49844e098bd
Greysen <gr10689@email.vccs.edu>
2024-10-01 00:02:17 +0000

    wrote what the assignment wanted and added some header stuff

--- .ccls-cache/@home@runner@Chapter-2-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Project/main.cpp
@@ -3,11 +3,22 @@
 // Greysen Roy-Pascoe
 // 09/25/2024
 // Chapter 2 Programming Project
+// Program 2 (the car one)
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  
+  float driving_in_town = 23.5;
+  float driving_on_highway = 28.9;
+  int gas_tank = 20;
+
+  cout << "The distance the car can travel on one tank of gas when driven in town is " << driving_in_town * gas_tank << " miles." << endl;
+
+  cout << "The distance it can travel when driving on the highway is " << driving_on_highway * gas_tank << " miles." << endl;
+
+  cout << "WOW!\n";
+
+  return 0;
 }
